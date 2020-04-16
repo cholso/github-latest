49f1e59a2536f2176e490c05abeddb26de58e002
Chelsea Nayan <chelseanayan@gmail.com>
2020-04-15 23:24:18 -0700

    Submitting github timestamp activity

---
+++ github-latest.py
@@ -0,0 +1,15 @@
+import sys
+import json
+import requests
+
+# Use python github-latest.py cholso
+
+# Print out the timestamp of the first 'event' 
+if __name__=="__main__":
+    username = sys.argv[1]
+
+    response = requests.get(f"https://api.github.com/users/{username}/events")
+    events = json.loads(response.content)
+
+    print(events[0]['created_at'])
+
